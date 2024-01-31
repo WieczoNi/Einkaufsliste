f7484a28601963d4faddbe94c616e60a94674a04
n.wieczorek <n.wieczorek@WKS-LEGION-Y720>
2024-01-31 15:01:54 +0100

    Projektdateien hinzufügen.

---
+++ Einkaufsliste/Program.cs
@@ -0,0 +1,11 @@
+﻿List<string> Einkaufsliste =  new List<string>();
+Einkaufsliste.Add("Käse");
+Einkaufsliste.Add("Butter");
+Einkaufsliste.Add("Nutella");
+Einkaufsliste.Add("Wurst");
+Einkaufsliste.Add("Brötchen");
+
+foreach(string item in Einkaufsliste)
+{
+    Console.WriteLine(item);
+}
